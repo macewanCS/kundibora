a05d9d785be28b23b948c96f36a699f30757ae84
Julian Martinez del Campo <MartinezDelCampoJ@mymacewan.ca>
2014-03-02 23:53:48 -0700

    fix database prefix error

--- plugins/analytics/models/analytics.php
+++ plugins/analytics/models/analytics.php
@@ -8,38 +8,42 @@ class Analytics_Model extends Model {
 
     public function get_incidents_grouped_by_id()
     {
-        $sql = "SELECT COUNT( category.id ) AS count, category.category_title, category.category_color 
-                FROM category 
-                JOIN incident_category ON category.id=incident_category.category_id 
-                JOIN incident ON incident_category.incident_id=incident.id 
-                GROUP BY category.id";
+        $prefix = $this->db->table_prefix();
+        $sql = "SELECT COUNT( ".$prefix."category.id ) AS count, ".$prefix."category.category_title, ".$prefix."category.category_color 
+                FROM ".$prefix."category 
+                JOIN ".$prefix."incident_category ON ".$prefix."category.id=".$prefix."incident_category.category_id 
+                JOIN ".$prefix."incident ON ".$prefix."incident_category.incident_id=".$prefix."incident.id 
+                GROUP BY ".$prefix."category.id";
         return $this->db->query($sql);
     }
 
     public function get_categories( $category_id = -1 )
     {
+        $prefix = $this->db->table_prefix();
         if( $category_id == -1 )
         {
-            $sql = "SELECT category.id, category.category_title, category.category_color 
-                    FROM category";
+            $sql = "SELECT ".$prefix."category.id, ".$prefix."category.category_title, ".$prefix."category.category_color 
+                    FROM ".$prefix."category";
         }
         else
         {
-            $sql = "SELECT category.id, category.category_title, category.category_color 
-                    FROM category 
-                    WHERE category.id=".$category_id;
+            $sql = "SELECT ".$prefix."category.id, ".$prefix."category.category_title, ".$prefix."category.category_color 
+                    FROM ".$prefix."category 
+                    WHERE ".$prefix."category.id=".$prefix . $category_id;
         }
+
         return $this->db->query($sql);
     }
 
     public function get_incidents_by_category( $category_id )
     {
-            $sql = "SELECT COUNT( category.id ) AS count, category.id, category.category_title, category.category_color, incident.incident_date 
-                    FROM category 
-                    JOIN incident_category ON category.id=incident_category.category_id 
-                    JOIN incident ON incident_category.incident_id=incident.id 
-                    WHERE category.id=".$category_id." 
-                    GROUP BY incident.incident_date";
-            return $this->db->query($sql);
+        $prefix = $this->db->table_prefix();
+        $sql = "SELECT COUNT( ".$prefix."category.id ) AS count, ".$prefix."category.id, ".$prefix."category.category_title, ".$prefix."category.category_color, ".$prefix."incident.incident_date 
+                FROM ".$prefix."category 
+                JOIN ".$prefix."incident_category ON ".$prefix."category.id=".$prefix."incident_category.category_id 
+                JOIN ".$prefix."incident ON ".$prefix."incident_category.incident_id=".$prefix."incident.id 
+                WHERE ".$prefix."category.id=".$category_id." 
+                GROUP BY ".$prefix."incident.incident_date";
+        return $this->db->query($sql);
     }
 }
