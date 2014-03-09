891dee6c15cb4b2c5ceba30b92f0d1d58877e90c
Julian Martinez del Campo <MartinezDelCampoJ@mymacewan.ca>
2014-03-09 01:47:58 -0700

    Refactor chart rendering Add axis label flot plugin Add selection flot plugin

--- plugins/analytics/views/flot_js.php
+++ plugins/analytics/views/flot_js.php
@@ -5,6 +5,8 @@
 echo html::script( array(
     'plugins/analytics/media/js/jquery.flot.min',
     'plugins/analytics/media/js/jquery.flot.pie.min',
-    'plugins/analytics/media/js/jquery.flot.time.min'
+    'plugins/analytics/media/js/jquery.flot.time.min',
+    'plugins/analytics/media/js/jquery.flot.selection',
+    'plugins/analytics/media/js/jquery.flot.axislabels'
     ), FALSE); 
 ?>
