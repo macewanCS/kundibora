b45f0b34464d1948f5372fbe72f4ea2c9f294f0e
Julian Martinez del Campo <MartinezDelCampoJ@mymacewan.ca>
2014-03-12 22:24:49 -0600

    Fix bug in analytics view

--- plugins/analytics/views/analytics.php
+++ plugins/analytics/views/analytics.php
@@ -1,68 +1,68 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class analytics {
-	
-	/**
-	 * Registers the main event add method
-	 */
-	public function __construct()
-	{	
-		// Hook into routing
-		Event::add('system.pre_controller', array($this, 'add'));
-	}
-	
-	/**
-	 * Adds all the events to the main Ushahidi application
-	 */
-	public function add()
-		{
-		Event::add('ushahidi_action.nav_main_top', array($this, 'navigation_tab'));
-				Event::add('ushahidi_action.header_scripts', array($this, 'add_flotjs'));
-	}
-
-		/**
-		 * Create analytics navigation tab
-		 *
-		 * @param this_page show tabs on this page
-		 * @param dont show tabs
-		 */
-		public function navigation_tab($this_page = FALSE, $dontshow = FALSE)
-		{
-			$menu_items = array();
-
-			if( ! is_array($dontshow))
-			{
-					// Set $dontshow as an array to prevent errors
-					$dontshow = array();
-			}
-
-			if( ! in_array('analytics',$dontshow))
-			{
-					$menu_items[] = array( 
-							'page' => 'analytics',
-							'url' => url::site('analytics'),
-							'name' => Kohana::lang('ui_main.analytics')
-					);	
-			}
-
-			// create a new view
-			$analytics_tab = View::factory( 'analytics_tab' );
-
-			// Populate tabs
-			$analytics_tab->menu_items = $menu_items;
-
-			// render view
-			$analytics_tab->render( TRUE );
-		}
-
-		/**
-		 * Add flot charts to ushahidi
-		 */
-		public function add_flotjs()
-		{
-			$js = View::factory('flot_js');
-			$js->render(TRUE);
-		}
+<script type="text/javascript"><?php echo "$chart"; ?></script>
+<style>
+/*
+#analytic-tabs ul {
+	padding: 1em;
+	clear: both;
 }
 
-new analytics;
+#analytic-tabs li {
+	list-style-type: none;
+	background-color: #c2c2c2;
+	float: left;
+	margin: 1em;
+	padding: 1em;
+}
+
+#analytic-tabs li:hover {
+	background-color: #008cff;
+}
+
+#analytic-tabs a {
+	color: #fff;
+	font-weight: bold;
+	text-decoration: none;
+}
+
+#analytic-tabs div {
+	text-align: left;
+	float: center;
+	clear: both;
+	width: 100%;
+}
+
+*/
+</style>
+<div id="content">
+	<div class="content-bg">
+		<!-- start analytics block -->
+		<div class="big-block">
+			<h1><?php echo Kohana::lang('ui_main.analytics'); ?></h1>
+						
+						<div id="analytic-tabs">
+							<ul>
+								<li><a id="analytics-tab-0" class="analytics-tab" href="#all-time">All Time</a></li>
+								<li><a id="analytics-tab-1" class="analytics-tab" href="#daily-incidents">Daily Incidents</a></li>
+								<li><a id="analytics-tab-2" class="analytics-tab" href="#total-incidents">Total Incidents</a></li>
+							</ul>
+
+							<div id="all-time">
+								<div id="chart-all-time" style="width:100%;height:600px;"></div>
+							</div>
+							<!-- end total-pie-chart block -->
+
+							<div id="daily-incidents">
+								<div id="chart-daily-incidents" style="width:800px;height:600px;"></div>
+							</div>
+							<!-- end total-incidents block -->
+
+							<div id="total-incidents">
+								<div id="chart-total-incidents" style="width:800px;height:600px;"></div>
+							</div>
+							<!-- end daily-incidents block -->
+						</div>
+						<!-- end analytic-tabs block -->
+		</div>
+		<!-- end analytics block -->
+	</div>
+</div>
