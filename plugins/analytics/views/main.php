a8561630eb21926b07963df3123d6459bd0142c3
Julian Martinez del Campo <MartinezDelCampoJ@mymacewan.ca>
2014-04-08 20:29:01 -0600

    Add filter header titles

--- plugins/analytics/views/main.php
+++ plugins/analytics/views/main.php
@@ -10,18 +10,23 @@
         <div id="chart-filter-box">
             <form name="filters">
                 <div id="chart-type-filter-box" class="filter-box">
+                    <h2>Chart Type</h2>
                     <?php echo analytics_helper::select_chart_type("radio", "chartType", "filter-element"); ?>
                 </div>
                 <div id="chart-cumulative-filter-box" class="filter-box">
+                    <h2>Daily or Cumulative incidents</h2>
                     <?php echo analytics_helper::select_cumulative("radio", "cumulative", "filter-element"); ?>
                 </div>
                 <div id="chart-category-filter-box" class="filter-box">
+                    <h2>Category</h2>
                     <?php echo analytics_helper::select_category("checkbox", "categoryId", "filter-element"); ?>
                 </div>
                 <div id="chart-country-filter-box" class="filter-box">
+                    <h2>Country</h2>
                     <?php echo analytics_helper::select_country("checkbox", "countryId", "filter-element"); ?>
                 </div>
                 <div id="chart-custom-filter-box" class="filter-box">
+                    <h2>Keyword</h2>
                     <label>Custom Filter
                         <input type="text" name="keyword" />
                     </label>
