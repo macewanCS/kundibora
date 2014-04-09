1d5e6edf31e0878f974fe122e42b90d10cf3c696
Julian Martinez del Campo <MartinezDelCampoJ@mymacewan.ca>
2014-04-09 12:29:27 -0600

    Add model for countries, categories, and incidents

--- plugins/analytics/models/analytics.php
+++ plugins/analytics/models/analytics.php
@@ -17,12 +17,12 @@ class Analytics_Model extends Model {
             $prefix = $this->db->table_prefix();    // get table prefix
 
             $sql = '';
-            $sql .= "SELECT ".$prefix."category.id as category_id, "
-                             .$prefix."category.category_title as category_title, "
-                             .$prefix."category.category_description as category_description, "
-                             .$prefix."category.category_color as category_color, "
-                             .$prefix."category.category_trusted as category_trusted, "
-                             .$prefix."category.parent_id as category_parent ";
+            $sql .= "SELECT ".$prefix."category.id AS category_id, "
+                             .$prefix."category.category_title AS category_title, "
+                             .$prefix."category.category_description AS category_description, "
+                             .$prefix."category.category_color AS category_color, "
+                             .$prefix."category.category_trusted AS category_trusted, "
+                             .$prefix."category.parent_id AS category_parent ";
             $sql .= "FROM ".$prefix."category ";
 
             if( ! empty( $category_id ) )
@@ -44,10 +44,10 @@ class Analytics_Model extends Model {
             $prefix = $this->db->table_prefix();    // get table prefix
 
             $sql = '';
-            $sql .= "SELECT ".$prefix."country.id as country_id, "
-                             .$prefix."country.country as country_name, "
-                             .$prefix."country.capital as country_capital, "
-                             .$prefix."country.cities as country_cities ";
+            $sql .= "SELECT ".$prefix."country.id AS country_id, "
+                             .$prefix."country.country AS country_name, "
+                             .$prefix."country.capital AS country_capital, "
+                             .$prefix."country.cities AS country_cities ";
             $sql .= "FROM ".$prefix."country ";
 
             if( ! empty( $category_id ) )
@@ -57,7 +57,78 @@ class Analytics_Model extends Model {
 
             return $this->db->query( $sql );
         }
+        
+        /**
+         * Get incidents by category
+         * 
+         * @param string $keyword a keyword to search for
+         * @param int $category_id a category to select
+         * @param bool $group_by_country group countries together
+         * @param datetime $date_from a date start from
+         * @param datetime $date_to a date to stop at
+         * @return incidents by category
+         */
+        public function get_incidents_by_category( $keyword = null, $category_id = null, $group_by_country = false, $date_from = null, $date_to = null)
+        {
+            $prefix = $this->db->table_prefix();
 
+            $sql = '';
+            $sql .= "SELECT COUNT( ".$prefix."incident.id ) AS incident_count, "
+                                    .$prefix."incident.location_id AS incident_location, "
+                                    .$prefix."incident.id AS incident_id, "
+                                    .$prefix."incident.incident_title AS incident_title, "
+                                    .$prefix."incident.incident_description AS incident_description, "
+                                    .$prefix."incident.incident_date AS incident_date, "
+                                    .$prefix."incident.incident_active AS incident_active, "
+                                    .$prefix."incident.incident_verified AS incident_verified, "
+                                    .$prefix."incident.location_id AS incident_location, ";
+            $sql .=  $prefix."category.id AS category_id, "
+                    .$prefix."category.category_title AS category_title, "
+                    .$prefix."category.category_description AS category_description, "
+                    .$prefix."category.category_color AS category_color, "
+                    .$prefix."category.category_trusted AS category_trusted ";
+            $sql .= "FROM ".$prefix."incident ";
+            $sql .= "LEFT JOIN ".$prefix."incident_category ".
+                    "ON ".$prefix."incident.id=".$prefix."incident_category.incident_id ".
+                    "LEFT JOIN ".$prefix."category ".
+                    "ON ".$prefix."incident_category.category_id=".$prefix."category.id ";
+
+            $is_start = true;
+            if( ! empty( $category_id ) )
+            {
+                $sql .= ($is_start ? " WHERE " : " AND ").$prefix."category.id=".$category_id." ";
+                $is_start = false;
+            }
+
+            if( ! empty( $date_from ) )
+            {
+                $sql .= ($is_start ? " WHERE " : " AND ").$prefix."incident_date >= '".$date_from."' ";
+                $is_start = false;
+            }
+
+            if( ! empty( $date_to ) )
+            {
+                $sql .= ($is_start ? " WHERE " : " AND ").$prefix."incident_date <= '".$date_to."' ";
+                $is_start = false;
+            }
+
+            if( ! empty( $keyword ) )
+            {
+                $sql .= ($is_start ? " WHERE " : " AND ").$prefix."incident_title LIKE '%".$keyword."%' ";
+                $is_start = false;
+            }
+
+            if( $group_by_country == true )
+            {
+                $sql .= "GROUP BY ".$prefix."incident.location_id ";
+            }
+            else
+            {
+                $sql .= "GROUP BY ".$prefix."incident.incident_date ";
+            }
+
+            return $this->db->query( $sql );
+        }
 
 
 	public function get_incidents_grouped_by_id()
@@ -89,7 +160,7 @@ class Analytics_Model extends Model {
 		return $this->db->query($sql);
 	}
 
-	public function get_incidents_by_category( $category_id )
+	public function _get_incidents_by_category( $category_id )
 	{
 		$prefix = $this->db->table_prefix();
 		$sql = "SELECT COUNT( ".$prefix."category.id ) AS count, ".$prefix."category.id, ".$prefix."category.category_title, ".$prefix."category.category_color, ".$prefix."incident.incident_date 
